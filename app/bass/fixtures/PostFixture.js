c774527c2bcc2ab1d908fff1cb9b9f5600d22b44
Marc Roulias <marc@marcroulias.com>
2017-08-25 22:39:41 -0700

    added initial yml fixtures

--- app/bass/fixtures/PostFixture.js
+++ app/bass/fixtures/PostFixture.js
@@ -34,6 +34,29 @@ module.exports = class PostFixture extends AbstractFixture {
 
         const manager = this.getManager();
 
+        this.mapFromYmlDirectory(path.join(__dirname, 'data', 'posts'), (model, row, idx, cb) => {
+
+            model.referenceId = row.id;
+            model.user = this.getReference('user-' + row.user_id);
+            model.categories = row.categories;
+            model.tags = row.tags;
+            model.title = row.title;
+            model.body = row.body;
+            model.author = row.author;
+
+            // add a reference to this user so other fixtures can reference it
+            this.addReference('post-' + row.id, model);
+
+            manager.persist(model);
+
+        }, () => {
+
+            manager.flush().then(next).catch(err => {
+                console.error(err.stack || err);
+            });
+
+        });
+
         // map the comments first
         // const comments = {};
         // this.mapFromFile(path.join(__dirname, 'data', 'CommentFixture.csv'), (model, row, idx, cb) => {
@@ -62,54 +85,55 @@ module.exports = class PostFixture extends AbstractFixture {
         // }
 
         // map the posts
-        this.mapFromFile(path.join(__dirname, 'data', 'PostFixture.csv'), (model, row, idx, cb) => {
+        // this.mapFromFile(path.join(__dirname, 'data', 'PostFixture.csv'), (model, row, idx, cb) => {
+        //
+        //     model.referenceId = row.id;
+        //     model.user = this.getReference('user-' + row.user_id);
+        //     model.categories = Function('return ' + row.categories)();
+        //     model.tags = Function('return ' + row.tags)();
+        //     model.title = row.title;
+        //     model.body = row.body;
+        //     model.author = row.author;
+        //
+        //     // if (Math.random() * 10 < 7) {
+        //     //     let numComments = Math.floor(Math.random() * 10);
+        //     //     while (numComments > 0) {
+        //     //         let comment;
+        //     //         do {
+        //     //             comment = this.getReference('comment-' +
+        //     //                 (Math.max(1, Math.floor(Math.random() * 1000)))
+        //     //             );
+        //     //         } while (comment && comment.__assigned);
+        //     //         if (comment) {
+        //     //             comment.__assigned = true;
+        //     //             if (comment.__hasNested) {
+        //     //                 for (let child of comment.comments) {
+        //     //                     manager.persist(child);
+        //     //                 }
+        //     //             }
+        //     //             manager.persist(comment);
+        //     //             model.comments.push(comment);
+        //     //         }
+        //     //     }
+        //     // } else if (Math.random() * 5 > 3) {
+        //     //     model.options.nestedComments = false;
+        //     // } else {
+        //     //     model.options.comments = false;
+        //     // }
+        //
+        //     // add a reference to this user so other fixtures can reference it
+        //     this.addReference('post-' + row.id, model);
+        //
+        //     manager.persist(model);
+        //
+        // }, () => {
+        //
+        //     manager.flush().then(next).catch(err => {
+        //         console.error(err.stack || err);
+        //     });
+        //
+        // });
 
-            model.referenceId = row.id;
-            model.user = this.getReference('user-' + row.user_id);
-            model.categories = Function('return ' + row.categories)();
-            model.tags = Function('return ' + row.tags)();
-            model.title = row.title;
-            model.body = row.body;
-            model.author = row.author;
-
-            // if (Math.random() * 10 < 7) {
-            //     let numComments = Math.floor(Math.random() * 10);
-            //     while (numComments > 0) {
-            //         let comment;
-            //         do {
-            //             comment = this.getReference('comment-' +
-            //                 (Math.max(1, Math.floor(Math.random() * 1000)))
-            //             );
-            //         } while (comment && comment.__assigned);
-            //         if (comment) {
-            //             comment.__assigned = true;
-            //             if (comment.__hasNested) {
-            //                 for (let child of comment.comments) {
-            //                     manager.persist(child);
-            //                 }
-            //             }
-            //             manager.persist(comment);
-            //             model.comments.push(comment);
-            //         }
-            //     }
-            // } else if (Math.random() * 5 > 3) {
-            //     model.options.nestedComments = false;
-            // } else {
-            //     model.options.comments = false;
-            // }
-
-            // add a reference to this user so other fixtures can reference it
-            this.addReference('post-' + row.id, model);
-
-            manager.persist(model);
-
-        }, () => {
-
-            manager.flush().then(next).catch(err => {
-                console.error(err.stack || err);
-            });
-
-        });
     }
 
 }
